f669e158b69e420d99366fb9d186281e3f8b7b83
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2024-04-25 01:20:13 -0300

    Update ejercicio_1.sql

--- models/example/ejercicio_1.sql
+++ models/example/ejercicio_1.sql
@@ -1,2 +1,57 @@
 {{ config(materialized='view') }}
-SELECT * FROM DBT.AVENU_SCHEMA.BUSINESS
+select * from (
+
+WITH AllYearMonths AS (
+    SELECT DISTINCT YEAR(issued_at) AS year, MONTH(issued_at) AS month
+    FROM DBT.AVENU_SCHEMA.INVOICE
+    WHERE issued_at > (
+        SELECT DATEADD(month, -24, MAX(issued_at))
+        FROM DBT.AVENU_SCHEMA.INVOICE
+        WHERE TYPE = 'I'
+    )
+),
+Issuers AS (
+    SELECT DISTINCT ISSUER_RFC
+    FROM DBT.AVENU_SCHEMA.INVOICE
+    WHERE TYPE = 'I'
+)
+SELECT 
+    AYM.year,
+    AYM.month,
+    Issuers.ISSUER_RFC,
+    COALESCE(COUNT(invoice.id), 0) AS num_invoices
+FROM 
+    AllYearMonths AYM
+CROSS JOIN
+    Issuers
+LEFT JOIN 
+    (
+        SELECT 
+            YEAR(issued_at) AS year,
+            MONTH(issued_at) AS month,
+            ISSUER_RFC,
+            id
+        FROM 
+            DBT.AVENU_SCHEMA.INVOICE
+        WHERE 
+            TYPE = 'I'
+    ) invoice
+ON 
+    AYM.year = invoice.year
+    AND AYM.month = invoice.month
+    AND Issuers.ISSUER_RFC = invoice.ISSUER_RFC
+GROUP BY
+    AYM.year,
+    AYM.month,
+    Issuers.ISSUER_RFC
+ORDER BY
+    AYM.year DESC,
+    AYM.month DESC);
+
+
+
+
+
+
+
+
