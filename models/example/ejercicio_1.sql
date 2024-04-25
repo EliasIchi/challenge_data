e7e91fb8c25653e8ff4e242e1ae54ccc31b3758e
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2024-04-25 01:29:20 -0300

    Update ejercicio_1.sql

--- models/example/ejercicio_1.sql
+++ models/example/ejercicio_1.sql
@@ -1,21 +1,49 @@
-INSERT INTO MY_DB.MY_SCHEMA_REPLICA_PROD.BUSINESS (ID, CUSTOMER_ID, STATUS, NAME, TAX_ID, REGISTRATION_NUMBER, TYPE, ADDRESS, _FIVETRAN_DELETED, _FIVETRAN_SYNCED) 
-(
-  SELECT
-  'USMYMTRPZ8',
-  '63a35215-a42d-4233-a62f-7f9430180314',
-  'active', 
-  'GGO3P8BTFE DRGFJ5D4AH', 
-  'USMYMTRPZ8', 
-  'USMYMTRPZ8', 
-  'exporter', 
-  TO_VARIANT('{
-  "city": "SALTILLO",
-  "country": "CO",
-  "number": "457",
-  "state": "Coahuila",
-  "street": "NUBES DEL CIELO",
-  "zipCode": "258"
-   }'),
-  null,
-  '2021-10-06 23:44:03.169000000 +00:00'
+{{ config(materialized='view') }}
+select * from (
+
+WITH AllYearMonths AS (
+    SELECT DISTINCT YEAR(issued_at) AS year, MONTH(issued_at) AS month
+    FROM DBT.AVENU_SCHEMA.INVOICE
+    WHERE issued_at > (
+        SELECT DATEADD(month, -24, MAX(issued_at))
+        FROM DBT.AVENU_SCHEMA.INVOICE
+        WHERE TYPE = 'I'
+    )
+),
+Issuers AS (
+    SELECT DISTINCT ISSUER_RFC
+    FROM DBT.AVENU_SCHEMA.INVOICE
+    WHERE TYPE = 'I'
 )
+SELECT 
+    AYM.year,
+    AYM.month,
+    Issuers.ISSUER_RFC,
+    COALESCE(COUNT(invoice.id), 0) AS num_invoices
+FROM 
+    AllYearMonths AYM
+CROSS JOIN
+    Issuers
+LEFT JOIN 
+    (
+        SELECT 
+            YEAR(issued_at) AS year,
+            MONTH(issued_at) AS month,
+            ISSUER_RFC,
+            id
+        FROM 
+            DBT.AVENU_SCHEMA.INVOICE
+        WHERE 
+            TYPE = 'I'
+    ) invoice
+ON 
+    AYM.year = invoice.year
+    AND AYM.month = invoice.month
+    AND Issuers.ISSUER_RFC = invoice.ISSUER_RFC
+GROUP BY
+    AYM.year,
+    AYM.month,
+    Issuers.ISSUER_RFC
+ORDER BY
+    AYM.year DESC,
+    AYM.month DESC);
